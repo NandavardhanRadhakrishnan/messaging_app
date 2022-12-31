7259fccafad886fb27b05383ef67673304ac00e0
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2022-12-31 21:52:48 +0530

    first convo was possible

---
+++ lib/screens/ChatBubble.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:messaging_app/models/chatModel.dart';
+import 'package:messaging_app/models/userModel.dart';
+
+class ChatBubble extends StatelessWidget {
+  bool isRight;
+  Chat chat;
+  ChatBubble({required this.isRight, required this.chat, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(),
+    );
+  }
+}
